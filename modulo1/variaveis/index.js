e6b3323d82371d551bcc2532a86bc9679b2a679c
Leandro Ferreira <leandroveter@gmail.com>
2022-02-14 16:54:08 -0300

    index.js

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,51 @@
+/* 
+Resposta da pergunta 1 = 
+
+10 
+                        
+10, 5
+
+Resposta da pergunta 2 =
+
+10, 10, 10
+
+Resposta da pergunta 3 =
+
+let horasTrabalhadasDia = prompt ("Quantas horas você trabalha por dia?");
+let pagamentoDia = prompt("Quanto você recebe por dia?");
+alert("pagamentoDia/horasTrabalhadasDia");        */
+
+/*Escrita do código*/
+
+let nome;
+let idade;
+
+console.log("a variável 'nome' é", typeof nome)
+console.log("a variável 'idade' é", typeof idade)
+
+/*Não consegui visualizar o tipo impresso, mas imagino que seja o undefined pois não possuem valores*/
+
+nome = prompt("Qual é o seu nome?");
+idade = prompt("Quan é a sua idade");
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+/* */
+
+console.log("Olá,", nome, "Você tem" idade "anos");
+
+
+/* 2 exercício de escrita de códigos*/
+
+let statusLocalização = prompt("Você está em casa?");
+let statusTrabalho = prompt ("Você está trabalhando?");
+let StatusDisposição = prompt ("Você está disposto?");
+
+console.log ("Você está em casa?", statusLocalização);
+console.log("Você está traballhando?", statusTrabalho);
+console.log("Você está disposto?", StatusDisposição);
+
+
+
+
