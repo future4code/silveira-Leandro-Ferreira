5d7dbefb4e21d2fe2f1d60b285c21836d3bcbaf9
Leandro Ferreira <leandroveter@gmail.com>
2022-02-21 16:58:31 -0300

    index.js

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,44 @@
+/* 
+Exercícios de interpretação de código:
+
+1)
+a.
+Mathews Nachtergaele
+Virginia Cavendish
+canal: "Globo", horário:"14"
+
+2)
+
+a. nome:"Juca", idade:3, raca:"SRD";
+        "Juba", idade:3, raca:"SRD";
+        "Jubo", idade:3, raca:"SRD".
+
+b. copia o array ou objeto.
+
+3)
+a.
+false
+undegined
+b.
+undefined porque não há a propriedade altura e nem foi requisitado a crianção.
+
+
+*/
+
+// Exercícios de escrita de código
+
+// 1)
+
+const pessoa = { 
+nome: "Juliano",
+apelidos: ["Juca, Juju, Jucas"]
+
+
+};
+
+// Vou realizar os exercícios de noite, tive problemas com  internet e temporais na cidade
+
+
+
+
+
