1910dbe4a76444257bbf045f5642048b45221e8a
Leandro Ferreira <leandroveter@gmail.com>
2022-02-23 16:35:09 -0300

    index.js

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,16 @@
+/*
+Exercícios de interpretação de código
+
+1)Ele cria uma variável com o valor inicial de zero 
+e depois executa o laço "for" até o valor de i chegar a 5 e parar. 
+Para isso o incremento foi de +1
+
+2)a. 19, 21, 23, 25, 27, 30
+b. Sim
+
+
+let numero = 0 
+for (let numero of lista){
+
+
+}
